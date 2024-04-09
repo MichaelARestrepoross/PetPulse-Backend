4356e6d0e590603894f5a756c120ca356d75c1cf
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2024-04-09 16:22:22 -0400

    Updated Controller get for pets to get allr eminders for pet

--- controllers/petsController.js
+++ controllers/petsController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const pets = express.Router();
 
+//qeury imports
 const {
   getAllPets,
   getPetById,
@@ -9,23 +10,28 @@ const {
   updatePetById
 } = require("../queries/pets");
 
+const {getAllReminders} = require("../queries/reminders")
+
+//validations
 const { validatePet } = require("../validations/petsValidation");
 
 pets.get("/", async (req, res) => {
   try {
-    const allPets = await getAllPets();
-    res.status(200).json(allPets);
-  } catch (error) {
-    res.status(500).json({ error: "Server error" });
-  }
+      const allPets = await getAllPets();
+      res.status(200).json(allPets);
+    } catch (error) {
+        res.status(500).json({ error: "Server error" });
+    }
 });
 
 pets.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const pet = await getPetById(id);
+    const { id } = req.params;
+    try {
+      const pet = await getPetById(id);
+      const allReminders = await getAllReminders();
+      const filterdReminders = allReminders.filter((reminder)=> pet.id === reminder.pet_id)
     if (pet) {
-      res.status(200).json(pet);
+      res.status(200).json({...pet, filterdReminders});
     } else {
       res.status(404).json({ error: "Pet not found with this ID" });
     }
